2c61ff0a9715c87ee1ec1a3d40cf0ff1921353d1
Vinby <48126197+Vinby@users.noreply.github.com>
2021-09-06 19:30:56 +0400

    stashed obsulite project folder

--- Application/Startup.cs
+++ Application/Startup.cs
@@ -54,6 +54,7 @@ namespace Application
             {
                 endpoints.MapControllers();
             });
+            
         }
     }
 }
--- Repository/ApplicationDbContext.cs
+++ Repository/ApplicationDbContext.cs
@@ -5,15 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System.Configuration;
+using Domain.Entity;
 
 
 namespace Repository
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Car> Cars { get; set; }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(System.Configuration)
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
         }
     }
 }
